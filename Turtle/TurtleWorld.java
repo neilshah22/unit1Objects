e644ac1eaa71a6ca1aefd976ca648d08513a9f09
neilshah22 <nnshah@NN123-HA-03.HS.sd203.org>
2015-08-20 14:12:20 -0500

    Tried to make the turtle draw a shape

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,18 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setPenColor(Color.BLUE);
         turtle.penDown();
         turtle.forward(50);
+        turtle.turn(35);
+        turtle.forward(45);
+        turtle.turn(120);
+        turtle.forward(45);
+        turtle.turn(35);
+        turtle.forward(50);
+        turtle.turn(35);
+        turtle.forward(45);
+        turtle.turn(120);
+        turtle.forward(45);
     }
 }
