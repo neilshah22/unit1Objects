8b04d1f8c61bfe06d10bb88a203cb737628ca69b
neilshah22 <nnshah@NN123-HA-03.HS.sd203.org>
2015-08-24 14:12:56 -0500

    i wrote hello world

---
+++ Chapter one practice/HelloPrinter.java
@@ -0,0 +1,9 @@
+public class HelloPrinter
+{
+    public static void main( String[] args )
+    {
+        //display a greeting in the counsole window
+        
+        System.out.println( "Hello, World!" );
+    }
+}
